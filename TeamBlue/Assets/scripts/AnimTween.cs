cc0a81e466298341745c8a81d7b137f0c54dd7ba
Herman Dempere <nerucius@gmail.com>
2016-03-16 17:50:28 +0100

    Animation system

---
+++ TeamBlue/Assets/scripts/AnimTween.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+
+public class AnimTween {
+
+    public GameObject obj;
+    public Vector3 from;
+    public Vector3 to;
+    public float progress;
+
+    public AnimTween() {
+        
+    }
+
+    public void processQueue() {
+
+    }
+
+    public void addToQueue(GameObject o, Vector3 from, Vector3 to) {
+        AnimParams newAnim;
+        newAnim.obj = o;
+        newAnim.from = from;
+        newAnim.to = to;
+    }
+
+
+    struct AnimParams {
+        public GameObject obj;
+        public Vector3 from;
+        public Vector3 to;
+        public float progress;
+    }
+}
+

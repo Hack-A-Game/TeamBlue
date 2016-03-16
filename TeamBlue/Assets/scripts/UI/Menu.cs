cf043ed50dc7479b5ce8dab799d8ab7d55cb0297
dtelloas <dtelloas7@alumnes.ub.edu>
2016-03-16 10:41:57 +0100

    end ui develop

--- TeamBlue/Assets/scripts/UI/Menu.cs
+++ TeamBlue/Assets/scripts/UI/Menu.cs
@@ -16,18 +16,30 @@ public class Menu : MonoBehaviour {
     void OnGUI()
     {
         // Make a background box
-        GUI.Box(new Rect(10, 10, 100, 90), "Loader Menu");
+        GUI.Box(new Rect(0, 0, 120, 140), "CHOOSE ACTION");
 
-        // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-        if (GUI.Button(new Rect(20, 40, 80, 20), "Level 1"))
+        // Make the Card1 button. If it is pressed, Application.Loadlevel (1) will be executed
+        if (GUI.Button(new Rect(10, 30, 100, 20), "Card 1"))
         {
-            Application.LoadLevel(1);
+            //Application.LoadLevel(1);
         }
 
-        // Make the second button.
-        if (GUI.Button(new Rect(20, 70, 80, 20), "Level 2"))
+        // Make the Card2 button.
+        if (GUI.Button(new Rect(10, 55, 100, 20), "Card 2"))
         {
-            Application.LoadLevel(2);
+            //Application.LoadLevel(2);
+        }
+
+        // Make the Swap button.
+        if (GUI.Button(new Rect(10, 80, 100, 20), "Swap"))
+        {
+            //Application.LoadLevel(3);
+        }
+
+        // Make the Pass button. If it is pressed, Application.Loadlevel (4) will be executed
+        if (GUI.Button(new Rect(10, 105, 100, 20), "Pass"))
+        {
+            //Application.LoadLevel(4);
         }
     }
 

efccd15d7f3095a3727d661e53d4e090d893a148
dtelloas <dtelloas7@alumnes.ub.edu>
2016-03-16 09:26:58 +0100

    UI Init

---
+++ TeamBlue/Assets/scripts/CameraMobAdjust.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMobAdjust : MonoBehaviour {
+
+    public Camera Cam1;
+
+	// Use this for initialization
+	void Start () {
+
+        Cam1.aspect = (Screen.currentResolution.width / Screen.currentResolution.height);
+        //This would stretch the game scene in order to adjust it to the Device's screen
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

9516dbacef74264497879de9a355c76390b8f8d6
sergicebriangres <sergicebriangres@gmail.com>
2016-03-16 10:30:51 +0100

    units, cards, deckBuilder and Deck added

---
+++ TeamBlue/Assets/scripts/Card.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+	public int ID;
+	public int cost;
+	public int health;
+	public int attack;
+	public element ele;
+	public string unitName;
+	public Sprite cardSprite;
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public enum element { water, fire, earth, neutral };
---
+++ TeamBlue/Assets/scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	private Deck deckPlayer;
+	private Deck deckCPU;
+
+	// Use this for initialization
+	void Start () {
+		//DeckBuilder.initDeck(deckPlayer);
+		//DeckBuilder.initDeck(deckCPU);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

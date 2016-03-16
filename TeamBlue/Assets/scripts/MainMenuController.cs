5794c374099649a86c99c30dc22fe68ceaad9154
Pablo Martínez <pmartima22@gmail.com>
2016-03-16 12:51:56 +0100

    Added main scene

---
+++ TeamBlue/Assets/scripts/MainMenuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void NewGameButton(){
+		Debug.Log ("NEW GAME");
+		Application.LoadLevel("Game");
+
+	}
+
+	public void ExitGameButton(){
+		Debug.Log ("EXIT");
+		Application.Quit ();
+	}
+}

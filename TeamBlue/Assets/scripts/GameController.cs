18ee2b0cecd7ac86255b1cad6199fb1e19444c78
Herman Dempere <nerucius@gmail.com>
2016-03-16 11:28:23 +0100

    Added GameController

--- TeamBlue/Assets/scripts/GameController.cs
+++ TeamBlue/Assets/scripts/GameController.cs
@@ -2,14 +2,46 @@
 using System.Collections;
 
 public class GameController : MonoBehaviour {
-	private Deck deckPlayer;
-	private Deck deckCPU;
+
+    private ArrayList playerHand;
+	private ArrayList cpuHand;
+
+    private DeckBuilder deckBuilder;
+
+    private Board board;
 
 	// Use this for initialization
 	void Start () {
-		//DeckBuilder.initDeck(deckPlayer);
-		//DeckBuilder.initDeck(deckCPU);
+
+        board = GameObject.Find("Board").GetComponent<Board>();
+        deckBuilder = GetComponent<DeckBuilder>();
+
+        // Inint player hands
+        playerHand = new ArrayList();
+        cpuHand = new ArrayList();
+        for (int i = 0; i < 3; i++) {
+            playerHand.Add(deckBuilder.newCard());
+            cpuHand.Add(deckBuilder.newCard());
+        }
+
+
 	}
+
+    // Returns true if a player can play a card
+    public bool canPlayCard(int player) {
+        switch (player) {
+            case 0:
+                return board.cardAt(0) == null;
+            case 1:
+                return board.cardAt(board.maxCards-1) == null;
+            default:
+                return false;
+        }
+    }
+
+    public ArrayList getPlayerHand() {
+        return playerHand;
+    }
 	
 	// Update is called once per frame
 	void Update () {
